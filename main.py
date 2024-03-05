8bbf32838750d1cbe4dd564dc74d17e971da3ab6
Joao Paulo Leal <jpll13@hotmail.com>
2024-03-05 16:37:06 -0300

    All done

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ pyautogui.write(link)
 pyautogui.press('enter')
 
 #preencher email e senha
-time.sleep(3)
+time.sleep(1)
 pyautogui.click(x = 800, y= 463)
 pyautogui.write("joaopaulo@gmail.com")
 pyautogui.press('tab')
@@ -23,10 +23,27 @@ pyautogui.write('senha123')
 pyautogui.press('enter')
 
 #buscar arquivos 
-time.sleep(3)
-tabela = pandas.read_csv("produtos.csv")
 
+tabela = pandas.read_csv("produtos.csv")
+time.sleep(1)
 
 #preencher e enviar
-for x in tabela.index:
-    pass
+for linha in tabela.index:
+    pyautogui.click(x=816, y=319)
+    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press('tab')
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press('tab')
+    if not pandas.isna(tabela.loc[linha, "obs"]):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    time.sleep(0.5)
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
